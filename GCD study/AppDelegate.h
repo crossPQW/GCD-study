703b0259aef448272bac601823f73af8584c3b49
crossPQW <651024752@qq.com>
2016-01-17 23:57:58 +0800

    Initial Commit

---
+++ GCD study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD study
+//
+//  Created by 黄少华 on 16/1/17.
+//  Copyright © 2016年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD study
+//
+//  Created by 黄少华 on 16/1/17.
+//  Copyright © 2016年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
